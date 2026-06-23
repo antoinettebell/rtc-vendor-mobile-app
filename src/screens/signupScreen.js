370d460f2231bd202ab8ee426317fddd2f25136c
chetanbhadarka <chetanbhadarka02@gmail.com>
2026-06-23 17:14:13 -0400

    Update SMS consent terms

--- src/screens/signupScreen.js
+++ src/screens/signupScreen.js
@@ -937,7 +937,7 @@ const SignUpScreen = ({ navigation }) => {
 
                 <Text style={styles.termsText}>
                   {
-                    "I agree to receive recurring automated text messages (eg. Cart reminders, Account updates) at the phone number provided. Consent is not a condition to purchase. Msg & data rates may apply. Reply HELP for help and STOP to cancel. View our "
+                    "I agree to receive automated transactional text messages from Round the Corner, including order confirmations, ready-for-pickup alerts, delivery updates, account updates, and order completion notices at the mobile number provided. Consent is not a condition of purchase. Message and data rates may apply. Message frequency varies. Reply HELP for help and STOP to cancel. View our "
                   }
                   <Text
                     style={styles.linkText}
