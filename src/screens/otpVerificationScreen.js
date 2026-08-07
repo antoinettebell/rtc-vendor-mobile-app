257d6763c8e8a27644dd6115930b01b060558df3
Round Da Corner <rounddacorner@Rounds-MacBook-Pro.local>
2026-08-07 00:01:42 -0400

    Use latest token after resending vendor OTP

--- src/screens/otpVerificationScreen.js
+++ src/screens/otpVerificationScreen.js
@@ -157,10 +157,13 @@ const OtpVerificationScreen = ({ route }) => {
         setResendTimer(RESEND_CODE_TIME);
         inputRefs.current[0]?.focus();
 
-        setParams({
-          ...params,
-          otpVerificationToken: resendResponse?.data?.otpVerificationToken,
-        });
+        setParams((current) => ({
+          ...current,
+          data: {
+            ...current?.data,
+            otpVerificationToken: resendResponse?.data?.otpVerificationToken,
+          },
+        }));
 
         setSnackbar({
           visible: true,
